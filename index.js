adbf5c37634d0bfacdff9078fa697cc0ecc03a34
erin.miller <erinm1018@gmail.com>
2024-09-29 00:33:15 -0500

    simplify: indexjs

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-// import FishCalculator from './js/fish.js';
-// import FlowersCalculator from './js/flowers.js';
-import GolfCalculator from './js/golf.js';
-import RacingCalculator from './js/racing.js';
-import SuitsCalculator from './js/suits.js';
-
-export {
-//   FishCalculator,
-//   FlowersCalculator,
-  GolfCalculator,
-  RacingCalculator,
-  SuitsCalculator,
-};
+// index.js
+// export { default as FishCalculator } from './js/fish.js';
+// export { default as FlowersCalculator } from './js/flowers.js';
+export { default as GolfCalculator } from './js/golf.js';
+export { default as RacingCalculator } from './js/racing.js';
+export { default as SuitsCalculator } from './js/suits.js';
