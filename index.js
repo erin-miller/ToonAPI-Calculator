b335a0c5edd5f2a84a6e05b68b3a9d26f0e04f19
erin.miller <erinm1018@gmail.com>
2024-09-29 14:42:50 -0500

    add: sort by best rarity

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // index.js
-// export { default as FishCalculator } from './js/fish.js';
+export { default as FishCalculator } from './js/fish.js';
 // export { default as FlowersCalculator } from './js/flowers.js';
 export { default as GolfCalculator } from './js/golf.js';
 export { default as RacingCalculator } from './js/racing.js';
 export { default as SuitsCalculator } from './js/suits.js';
+
+import FishCalculator from './js/fish.js';
+
+const calc = new FishCalculator();
+const best = calc.sortBestRarity();
+best.forEach(fish => {
+    const perc = (fish.probability * 100).toFixed(2);
+    console.log(`${fish.name}: ${perc}%`);
+})
