f7405a884e9bcf9638f98cc560092eab37f168c9
Erin Miller <erinm1018@gmail.com>
2024-09-29 21:35:19 -0700

    Add: fish calculator (#6)
    * fix: pathing:
    
    * update: package and pckg lock
    
    * update: package
    
    * update: to reflect api return
    
    * add: sort by best rarity
    
    * complete: fish calc
    
    * fix: testing
    
    * add: jsdoc
    
    * refactor: getBestLocation/Rarity and add jsdoc
    
    * release: 1.1.0, fish calc

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // index.js
-// export { default as FishCalculator } from './js/fish.js';
+export { default as FishCalculator } from './js/fish.js';
 // export { default as FlowersCalculator } from './js/flowers.js';
 export { default as GolfCalculator } from './js/golf.js';
 export { default as RacingCalculator } from './js/racing.js';
