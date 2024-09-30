829d75e8b89783bf4785c5dc429e8c920b45b078
erin.miller <erinm1018@gmail.com>
2024-09-29 23:05:16 -0500

    fix: testing

--- index.js
+++ index.js
@@ -4,15 +4,3 @@ export { default as FishCalculator } from './js/fish.js';
 export { default as GolfCalculator } from './js/golf.js';
 export { default as RacingCalculator } from './js/racing.js';
 export { default as SuitsCalculator } from './js/suits.js';
-
-import FishCalculator from './js/fish.js';
-
-const calc = new FishCalculator();
-const best = calc.sortBestRarity();
-Object.entries(best).forEach(([location, fishArray]) => {
-    fishArray.forEach(fish => {
-        const perc = (fish.probability * 100).toFixed(2);
-        console.log(`${fish.name} (${location}): ${perc}%`);
-    });
-});
-
