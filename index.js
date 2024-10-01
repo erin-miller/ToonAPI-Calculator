21e94c7751d52dcde8a61bd6a6695781979857a0
erin.miller <erinm1018@gmail.com>
2024-09-30 22:05:23 -0500

    v1.1.5

--- index.js
+++ index.js
@@ -4,10 +4,3 @@ export { default as FishCalculator } from './js/fish.js';
 export { default as GolfCalculator } from './js/golf.js';
 export { default as RacingCalculator } from './js/racing.js';
 export { default as SuitsCalculator } from './js/suits.js';
-
-import RacingCalculator from './js/racing.js';
-
-const toon = '[{"name": "Speedway Wins", "num": 1}, {"name": "Rural Wins", "num": 3}, {"name": "Urban Wins", "num": 4}, {"name": "Total Wins", "num": 3}, {"name": "Speedway Qualify Count", "num": 2}, {"name": "Rural Qualify Count", "num": 4}, {"name": "Urban Qualify Count", "num": 6}, {"name": "Total Qualify Count", "num": 4}, {"name": "Tournament Race Wins", "num": 4}, {"name": "Tournament Race Qualify Count", "num": 3}, {"name": "Unique race tracks completed", "num": 2}]';
-const race = new RacingCalculator(toon);
-
-console.log(race.getBestTrophy());
