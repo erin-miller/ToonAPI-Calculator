18cbd677e34b2180ea3978fed540b6fabedb00c6
erin.miller <erinm1018@gmail.com>
2024-10-01 20:03:11 -0500

    fix: test code

--- index.js
+++ index.js
@@ -4,12 +4,3 @@ export { default as FishCalculator } from './js/fish.js';
 export { default as GolfCalculator } from './js/golf.js';
 export { default as RacingCalculator } from './js/racing.js';
 export { default as SuitsCalculator } from './js/suits.js';
-
-import FishCalculator from './js/fish.js';
-
-const toon = `{"rod":{"id":0,"name":"Twig"},"collection":{}}`;
-const fishCalc = new FishCalculator(toon);
-const fish = fishCalc.sortBestRarity()
-fish.forEach(fish => {
-    console.log(`${fish.name}: ${fish.buckets} buckets (${(fish.probability*100).toFixed(2)}%) in ${fish.location}\n`);
-})
