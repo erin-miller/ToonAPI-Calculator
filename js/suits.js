8e236b5f86152a5b501cf5da68ad61a18f327c96
Erin Miller <erinm1018@gmail.com>
2024-10-03 19:27:29 -0700

    add: suit weighting based on time (#29)
    * add: building weight
    
    * sort: by weight
    
    * update: facility vals
    
    * add: option for weighted or not
    
    * fix: private methods at bottom
    
    * v1.4.0

--- js/suits.js
+++ js/suits.js
@@ -16,17 +16,6 @@ export default class SuitsCalculator {
         this.toon = JSON.parse(data);
     }
 
-    #getFacilityData(department) {
-        /**
-         * Grabs facility data based off of department.
-         *
-         * @param {string} department - Value in [c,l,m,s]
-         * @returns The corresponding department's facility information and values
-         */
-        const facility = this.suits_info.suitTypes[department].facility;
-        return this.suits_info.facilities[facility];
-    }
-
     getCurrent(department) {
         /**
          * @param {string} department - Value in [c,l,m,s]
@@ -49,6 +38,75 @@ export default class SuitsCalculator {
          * @returns Toon's needed experience to promo
          */
         return this.getTarget(department) - this.getCurrent(department);
+    }
+
+    getBestPathWeighted(department) {
+        /**
+         * Returns the optimal facility path to get to promotion, with facilities weighted by time.
+         *
+         * @param {string} department - Value in [c,l,m,s]
+         * @returns {Object[]} - JSON with optimal path and total value
+         *   If toon is maxed or has promotion, it will return with a message.
+         *   If toon does not have a disguise, it will return with a message.
+         */
+        const facilityInfo = this.#getFacilityData(department);
+        const toonInfo = this.toon[department];
+
+        if (toonInfo.hasDisguise) {
+            let path = [];
+            let total = 0;
+
+            const curr = toonInfo.promotion.current;
+            const target = toonInfo.promotion.target;
+            const remaining = target - curr;
+
+            if (remaining <= 0) {
+                return {
+                    path: [],
+                    total: 0,
+                    message: "The toon has no remaining promotion points needed."
+                };
+            }
+
+            const weighted = facilityInfo.sort((a,b) => {
+                return (b.value / b.weight) - (a.value / a.weight);
+            })
+
+            console.log(weighted);
+
+            for (const facility of weighted) {
+                const count = Math.floor((remaining - total) / facility.value);
+                
+                if (count > 0) {
+                    total += count * facility.value;
+                    // append count times
+                    path = path.concat(Array(count).fill(facility.name));
+                }
+                
+                if (total >= remaining) {
+                    break;
+                }
+            }
+            
+            // ensure remaining is overfilled
+            if (total < remaining) {
+                const overflow = weighted[weighted.length - 1];
+                path.push(overflow.name);
+                total += overflow.value;
+            }
+
+            return {
+                path: path,
+                total: total
+            };
+
+        } else {
+            return {
+                path: [],
+                total: -1,
+                message: "Toon does not have a disguise."
+            };
+        }
     }
 
     getBestPath(department) {
@@ -113,5 +171,17 @@ export default class SuitsCalculator {
             };
         }
     }
+
+    #getFacilityData(department) {
+        /**
+         * Grabs facility data based off of department.
+         *
+         * @param {string} department - Value in [c,l,m,s]
+         * @returns The corresponding department's facility information and values
+         */
+        const facility = this.suits_info.suitTypes[department].facility;
+        return this.suits_info.facilities[facility];
+    }
 }
+    
 
