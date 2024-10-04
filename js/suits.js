46dd20212a89768476ace9fa6e5c3f6b98514799
erin.miller <erinm1018@gmail.com>
2024-10-03 21:23:55 -0500

    add: option for weighted or not

--- js/suits.js
+++ js/suits.js
@@ -51,9 +51,9 @@ export default class SuitsCalculator {
         return this.getTarget(department) - this.getCurrent(department);
     }
 
-    getBestPath(department) {
+    getBestPathWeighted(department) {
         /**
-         * Returns the optimal facility path to get to promotion.
+         * Returns the optimal facility path to get to promotion, with facilities weighted by time.
          *
          * @param {string} department - Value in [c,l,m,s]
          * @returns {Object[]} - JSON with optimal path and total value
@@ -102,6 +102,69 @@ export default class SuitsCalculator {
             // ensure remaining is overfilled
             if (total < remaining) {
                 const overflow = weighted[weighted.length - 1];
+                path.push(overflow.name);
+                total += overflow.value;
+            }
+
+            return {
+                path: path,
+                total: total
+            };
+
+        } else {
+            return {
+                path: [],
+                total: -1,
+                message: "Toon does not have a disguise."
+            };
+        }
+    }
+
+    getBestPath(department) {
+        /**
+         * Returns the optimal facility path to get to promotion.
+         *
+         * @param {string} department - Value in [c,l,m,s]
+         * @returns {Object[]} - JSON with optimal path and total value
+         *   If toon is maxed or has promotion, it will return with a message.
+         *   If toon does not have a disguise, it will return with a message.
+         */
+        const facilityInfo = this.#getFacilityData(department);
+        const toonInfo = this.toon[department];
+
+        if (toonInfo.hasDisguise) {
+            let path = [];
+            let total = 0;
+
+            const curr = toonInfo.promotion.current;
+            const target = toonInfo.promotion.target;
+            const remaining = target - curr;
+
+            if (remaining <= 0) {
+                return {
+                    path: [],
+                    total: 0,
+                    message: "The toon has no remaining promotion points needed."
+                };
+            }
+
+            for (const facility of facilityInfo) {
+                const count = Math.floor((remaining - total) / facility.value);
+                
+                if (count > 0) {
+                    total += count * facility.value;
+                    // append count times
+                    path = path.concat(Array(count).fill(facility.name));
+                }
+                
+                if (total >= remaining) {
+                    break;
+                }
+            }
+            
+            // ensure remaining is overfilled
+            if (total < remaining) {
+                const overflow = facilityInfo[facilityInfo.length - 1];
                 path.push(overflow.name);
                 total += overflow.value;
             }
