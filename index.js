c33a209c504e39b2bc187b375356e16e51196436
erin.miller <erinm1018@gmail.com>
2024-11-02 20:53:40 -0500

    export: flowers calc

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // index.js
 export { default as FishCalculator } from './js/fish.js';
-// export { default as FlowersCalculator } from './js/flowers.js';
+export { default as FlowerCalculator } from './js/flowers.js';
 export { default as GolfCalculator } from './js/golf.js';
 export { default as RacingCalculator } from './js/racing.js';
 export { default as SuitsCalculator } from './js/suits.js';
